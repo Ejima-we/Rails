524b23872ec9d48d93828e910ff1d5ab6518cf6a
EC2 Default User <ec2-user@ip-172-31-54-84.ec2.internal>
2021-01-26 12:21:02 +0000

    [Finish]2-6

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,12 @@ Rails.application.routes.draw do
   get 'todolists/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "top" => "homes#top"
-  # postはデータベースを変更するアクション
+  # postはデータベースを変更するアクション　データが空の場合使用
   post "todolists" => "todolists#create"
   get "todolists" => "todolists#index"
   get "todolists/:id" => "todolists#show",as: "todolist"
-  get
+  get "todolists/:id/edit" =>"todolists#edit", as: "edit_todolist"
+  # patchは既存データがある場合に使用
+  patch "todolists/:id" => "todolists#update", as: "update_todolist"
 end
 
