68ee752bfaa0fecdcab53b8e2aa6b75bbe75f19d
EC2 Default User <ec2-user@ip-172-31-54-84.ec2.internal>
2021-01-27 06:59:22 +0000

    [Start]2-9

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -21,18 +21,18 @@ class TodolistsController < ApplicationController
     # 対象のデータをURL内のidから取得する
     @list = List.find(params[:id])
   end
-  
+
   def edit
     @list = List.find(params[:id])
   end
-  
+
   def update
     list = List.find(params[:id])
     # 編集したデータの更新
     list.update(list_params)
     redirect_to todolist_path(list.id)
   end
-  
+
   def destroy
     list = List.find(params[:id])
     list.destroy
